9d14879a85e08cb7935175302c71934e1ed4ce3b
RevanthGovindan <revanthji14@gmail.com>
2018-03-13 01:23:53 -0700

    mysql queries

---
+++ backend/gst.sql
@@ -0,0 +1,17 @@
+-- to create database:
+
+
+create database 'placementseason'
+
+
+-- to create table:
+
+
+
+CREATE TABLE Products (
+    product_code varchar(30),
+    product_name varchar(30),
+    product_price int,
+    product_code varchar(30),
+    PRIMARY KEY (product_code)
+);
